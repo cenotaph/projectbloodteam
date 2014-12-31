b6c9e3f10b7b6d20ab63adceb15db0b1902f8ad4
John W. Fail <fail@cenotaph.org>
2015-01-01 00:13:28 +0200

    fix nil error in categories

--- db/migrate/20141231215545_fix_firstview.rb
+++ db/migrate/20141231215545_fix_firstview.rb
@@ -1,7 +1,7 @@
 class FixFirstview < ActiveRecord::Migration
   def change
     execute(" update views set movie_first = '0' where movie_first is null")
-    execute("update views set movie_first = 'First?' where movie_first = ''")
-    execute("alter table views change movie_first movie_first varchar(255) not null")
+    execute("alter table views change movie_first movie_first varchar(255) not null default 'First'")
+    execute("update views set movie_first = 'First' where movie_first = ''")
   end
 end
