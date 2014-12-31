3efee08ce6039b7c8db2586855790984bbe0ecec
John W. Fail <fail@cenotaph.org>
2014-12-31 23:52:16 +0200

    trying to fxi currency bug

--- app/models/item_helpers.rb
+++ app/models/item_helpers.rb
@@ -47,8 +47,10 @@ module ItemHelpers
   end
   
   def check_currency_id
-    if self.currency_id.nil? && !self.date.nil?
-      self.currency_id = Profile.where(:year => self.date.strftime('%Y').to_i, :agent_id => self.agent_id).first.defaultcurrency_id
+    unless [Tvseries, Weight, Musicplayed].include?(self.class)
+      if self.currency_id.nil? && !self.date.nil?
+        self.currency_id = Profile.where(:year => self.date.strftime('%Y').to_i, :agent_id => self.agent_id).first.defaultcurrency_id
+      end
     end
   end
 
