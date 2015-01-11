a6dc42f300e48c1bb2398ca7accd64ae67cb470c
John W. Fail <fail@cenotaph.org>
2015-01-11 13:23:44 +0200

    time taken for tv

--- app/controllers/genericmaster_controller.rb
+++ app/controllers/genericmaster_controller.rb
@@ -335,7 +335,7 @@ class GenericmasterController < ApplicationController
           @choices = @master.classify.constantize.query(params[:query], session[:discogs_token]) #.delete_if{|x|         @localhits.map{|y| y.external_index}.include?(x['key'].to_i) }.each 
          end
       rescue Discogs::AuthenticationError
-        @choices = @master.classify.constantize.query(params[:query], session[:discogs_token])
+        @choices = []
       end
       if @choices && @localhits
         set_meta_tags :title => 'Searching for ' + params[:query]
