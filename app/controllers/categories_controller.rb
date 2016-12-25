54789a283b02cebe747b2e37c116936dc78c36f5
John W. Fail <john@pixelache.ac>
2016-12-25 22:42:40 +0200

    category edit fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -35,35 +35,35 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     if @category.agent != current_agent
       flash[:error] = 'This ain\'t your categories, bro.'
+      redirect_to '/agents/'
     else
  
      params[:category].each do |p|
-       if p[0] =~ /^has/
-         if p[1] == '0'
-            params['category'][p[0].sub(/^has_/,'')] = '0'
+      
+       if p =~ /^has/
+         if params[:category][p] == '0'
+           @category.update_column(p.sub(/^has_/,'').to_sym, 0)
+            # params['category'][p[0].sub(/^has_/,'')] = '0'
          else
-            params['category'][p[0].sub(/^has_/, '')] = params[:category][p[0].sub(/^has_/, '')]
+           @category.update_column(p.sub(/^has_/,'').to_sym, params[:category][p.sub(/^has_/, '')])
+            # params['category'][p[0].sub(/^has_/, '')] = params[:category][p[0].sub(/^has_/, '')]
          end
-         params[:category].delete(p[0])
+         # params[:category].delete(p[0])
         end
      end
-
- 
-   
-     if @category.update_attributes(category_params)
-       flash[:notice] = 'Edited ' + @category.year.to_s + ' categories.'
-        redirect_to '/settings'
-      else
-        flash[:notice] = 'You cannot edit another agent\'s category.'
-        redirect_to '/agents/'
-      end
+     flash[:notice] = 'Edited ' + @category.year.to_s + ' categories.'
+      redirect_to '/settings'
     end
   end
   
   private
   
   def category_params
-    params.require(:category).permit(:movies, :musics, :books, :bars, :restaurants, :concerts, :events, :eating, :exercise, :weight, :musicplayed, :activities, :airports, :videogames, :groceries, :brewing, :takeaway, :gambling, :tvseries, :year, :miles, :has_movies, :has_musics, :has_books, :has_activities, :has_airports, :has_brewing, :has_concerts, :has_eating, :has_events, :has_exercise, :has_gambling, :has_groceries, :has_miles, :has_musicplayed, :has_restaurants, :has_takeaway, :has_bars, :has_movies, :has_weight, :has_videogames, :has_tvseries)
+    params.require(:category).permit(:movies, :musics, :books, :bars, :restaurants, :concerts, :events, :eating, :exercise, :weight, 
+    :musicplayed, :activities, :airports, :videogames, :groceries,:brewing, :takeaway, :gambling, :tvseries, :year, :miles,
+     :has_movies, :has_musics, :has_books, :has_activities, :has_airports, :has_brewing, :has_concerts, :has_eating, :has_events, 
+     :has_exercise, :has_gambling, :has_groceries, :has_miles, :has_musicplayed, :has_restaurants, :has_takeaway, :has_bars, 
+     :has_weight, :has_videogames, :has_tvseries)
   end
   
 end
