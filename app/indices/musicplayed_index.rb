407f52f5414ccdcab0e8b40fbb67e573b334b157
John W. Fail <john@pixelache.ac>
2017-01-19 21:14:21 -0600

    sphinx config fix

--- app/indices/musicplayed_index.rb
+++ app/indices/musicplayed_index.rb
@@ -1,5 +1,5 @@
-#  # -*- encoding : utf-8 -*-
+# -*- encoding : utf-8 -*-
 ThinkingSphinx::Index.define :musicplayed, :with => :active_record, delta: true do
-  indexes name, location, comment
+  indexes company, location, comment
   has agent_id, created_at, updated_at
 end
