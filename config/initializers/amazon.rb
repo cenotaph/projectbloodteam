46f97d4cacf405197323cc5b91f56430f5bd13af
John W. Fail <john@pixelache.ac>
2017-06-10 16:46:27 +0300

    restor amazon

--- config/initializers/amazon.rb
+++ config/initializers/amazon.rb
@@ -1,6 +1,6 @@
 require 'amazon/ecs'
 Amazon::Ecs.configure do |options|
-  # options[:associate_tag] = ENV['AMAZON_ASSOC']
+  options[:associate_tag] = ENV['AMAZON_ASSOC']
   options[:AWS_access_key_id] = ENV['S3_ACCESS']
   options[:AWS_secret_key] =  ENV['S3_SECRET']
 end
