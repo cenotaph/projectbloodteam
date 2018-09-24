a28b788a0f017def4bf4a0cd1fc99cf6a95237cd
John W. Fail <john@pixelache.ac>
2018-09-24 11:46:47 +0300

    switched from google to openstreetmaps'

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,7 @@
 Geocoder.configure(
 
   # geocoding service (see below for supported options):
-  :lookup => :google
-
-
+  :lookup => :nominatim
 )
+
+Geocoder.configure(http_headers: { "User-Agent" => ENV['contact_email'] })
