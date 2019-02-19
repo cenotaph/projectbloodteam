2a50015c373d825b7f66fdbd307ce593db566ce4
John W. Fail <john@pixelache.ac>
2019-02-19 15:27:31 +0200

    closes rollbar 7

--- app/controllers/css_controller.rb
+++ app/controllers/css_controller.rb
@@ -7,11 +7,13 @@ class CssController < ApplicationController
   def show
     @agent = Agent.find(params[:id])
     profile = Profile.find_by(:agent => @agent, :year => getYear)
-    @css = profile.theme_settings
-
-    
-    respond_to do |format|
-      format.css {render :template => 'shared/agent_stylesheet' }
+    if profile.nil?
+      render plain: ''
+    else
+      @css = profile.theme_settings
+      respond_to do |format|
+        format.css {render :template => 'shared/agent_stylesheet' }
+      end
     end
   end
 
