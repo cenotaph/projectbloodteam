63ffd7e01d183ec0d5d62c6434b9c272a986841f
John W. Fail <john@pixelache.ac>
2020-08-15 22:39:27 +0300

    faster ajax lookups

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -6,15 +6,15 @@ class AjaxController < ApplicationController
     cat = params[:category].classify.constantize
     va = [Event, Movie, Activity, Musicplayed, Tvseries]
     if cat == Event || cat == Movie || cat == Activity || cat == Musicplayed || cat == Tvseries
-      items = cat.where('location like "%' + params[:term] + '%" and geolocation_id IS NOT NULL').group(:geolocation_id, :venue_address)
+      items = cat.joins(:geolocation_item).where('location like "%' + params[:term] + '%" and geolocation_items.id IS NOT NULL').group(:geolocation_id, :venue_address)
     elsif cat == Concert      
-      items = cat.where('venue like "%' + params[:term] + '%" and geolocation_id IS NOT NULL').group(:geolocation_id, :venue_address)
+      items = cat.joins(:geolocation_item).where('venue like "%' + params[:term] + '%" and geolocation_items.id IS NOT NULL').group(:geolocation_id, :venue_address)
     elsif cat == Brewing
-      items = cat.where('location like "%' + params[:term] + '%" and geolocation_id IS NOT NULL')
+      items = cat.joins(:geolocation_item).where('location like "%' + params[:term] + '%" and geolocation_items.id IS NOT NULL')
     else
-      items = cat.where('name like "%' + params[:term] + '%" and geolocation_id IS NOT NULL') #.group(:geolocation_id)
+      items = cat.joins(:geolocation_item).where('name like "%' + params[:term] + '%" and geolocation_items.id IS NOT NULL') #.group(:geolocation_id)
     end
-    items = items.to_a.delete_if{|u| u.geolocation.nil? }
+    items = items.to_a.delete_if{|u| u.geolocation_items.empty? }
     list = items.map{|u| Hash["id" => u.id, "label" => (va.include?(cat) ? u.location + " [" + u.geolocation.address.to_s + "]" : (cat == Concert ? u.venue + " [" + u.geolocation.address.to_s + "]" : u.name + " [" + u.location + "]")), "name" =>  (va.include?(cat) ? u.location  : (cat == Concert ? u.venue : ((cat == Movie || cat == Activity || cat == Event  || cat == Tvseries || cat == Musicplayed) ? u.location : u.name))), "lat" => u.geolocation.latitude, "lng" => u.geolocation.longitude, "geolocation_id" => u.geolocation_id, "address" => u.geolocation.address]}
 
     render :json => list.uniq(&:geolocation_id)
